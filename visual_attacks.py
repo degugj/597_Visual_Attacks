2452fcad123829a18a5219b44b779434e3d111ab
degugj <56598340+degugj@users.noreply.github.com>
2020-11-01 11:51:33 -0500

    Initial Commit
    Notes and sample code for Pi camera. See https://projects.raspberrypi.org/en/projects/getting-started-with-picamera/8

---
+++ visual_attacks.py
@@ -0,0 +1,18 @@
+from picamera import PiCamera
+from time import sleep
+
+# TODOs:
+# (1) Configure Pi Camera with Pi
+# (2) Still image attack - change/spoof aspects of an image
+# (3) Replay video attack - replay some duration of buffered frames
+# (4) "Active" video attack - change/spoof aspects of the video live
+
+# Notes:
+# If we're using the idea of colors as a verification technique, we should have some sort of range to identify them.
+# As Wei said, the NoIR camera is not going to be accurrate enough to detect the exact hexcode of a color
+
+camera = PiCamera()
+
+camera.start_preview()
+sleep(5)
+camera.stop_preview()
