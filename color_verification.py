5fe2fa97bad9e6fdb565de2f8db9bf3df0c82687
degugj <56598340+degugj@users.noreply.github.com>
2020-11-02 21:53:28 -0500

    Update color_verification.py

--- color_verification.py
+++ color_verification.py
@@ -1,19 +1,10 @@
-import io
 import time
 import picamera
-import cv2
 import numpy as np
 
-# Create the in-memory stream
-stream = io.BytesIO()
 with picamera.PiCamera() as camera:
-    camera.start_preview()
+    camera.resolution = (320, 240)
+    camera.framerate = 24
     time.sleep(2)
-    camera.capture(stream, format='jpeg')
-# Construct a numpy array from the stream
-data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-# "Decode" the image from the array, preserving colour
-image = cv2.imdecode(data, 1)
-# OpenCV returns an array with data in BGR order. If you want RGB instead
-# use the following...
-image = image[:, :, ::-1]
+    output = np.empty((240, 320, 3), dtype=np.uint8)
+    camera.capture(output, 'rgb')
