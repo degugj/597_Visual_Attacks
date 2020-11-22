f9488ca7180e1de7a4c5b8a1b2cb14b92c4301c3
achupanikath <achupanikath@gmail.com>
2020-11-22 00:23:06 -0500

    Inter frame correlation set up

---
+++ ifc.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+import statistics
+
+prev_frame = np.zeros((1080,1920,3), dtype = np.uint8)
+round0 = 1
+#put path to video below. might need to  reset datype based on video. check frame.dtype()
+cap = cv2.VideoCapture('Backend-Demo.mp4') 
+# ctr = 0
+maximumFrame = 0
+minCorr = 1000000
+sumArray = []
+correlationArray = []
+while(cap.isOpened()):
+	print(".")
+	if round0 == 0:
+		prev_frame = frame
+	ret, frame = cap.read()
+	if frame is None:
+		break
+	array_a = np.ndarray.flatten(frame)
+	array_b = np.ndarray.flatten(prev_frame)
+	corr = np.correlate(array_a, array_b)[0]
+	# print("Correlation: ",corr)
+	diff = cv2.absdiff(frame, prev_frame)
+	sumPixelArray = cv2.sumElems(diff)
+	# print("Prev Frame:",prev_frame)
+	# print("New Frame", frame)
+	# print("Diff",diff)
+	frameSum = cv2.sumElems(sumPixelArray)[0]
+	sumArray.append(frameSum)
+	if frameSum > maximumFrame:
+		maximumFrame = frameSum 
+	if	corr < minCorr:
+		minCorr = corr
+	correlationArray.append(corr)
+	mean = statistics.mean(sumArray)
+	color = cv2.cvtColor(frame, 0)
+	# cv2.imshow('frame diff ',diff)    
+	# cv2.imshow('frame',color)
+	round0 = 0
+	# ctr+=1
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+print("Maximum sum of difference between subsequent frames = ",maximumFrame)
+print("Minimum correlation = ", min(correlationArray))
+print("Maximum correlation = ", max(correlationArray))
+print("Correlation average = ", statistics.mean(correlationArray))
+print("Mean: ", mean)
+
+#put check condition based on the specific video that we will run for a certain amount of time
+
+
+cap.release()
+cv2.destroyAllWindows()
