5748d621c866d382c9a6345c3a0506261123d39c
degugj <jdeguglielmo@umass.edu>
2020-11-24 02:18:22 +0000

    selective replay attack implemented

--- replay_attack.py
+++ replay_attack.py
@@ -5,15 +5,32 @@ replayIt = 0
 
 """ replay_attack """
 def replay_attack(numFrames, frame):
+    if len(stored_frames) != numFrames:
+        # store current frame
+        stored_frames.append(frame)
+        return frame
+    else:
+        global replayIt
+        return_frame = stored_frames[replayIt]
+        replayIt += 1
+        if replayIt == numFrames:
+            replayIt = 0
+        return return_frame
+
+def selective_replay_attack(numFrames, frame):
         if len(stored_frames) != numFrames:
-                # store current frame
-                stored_frames.append(frame)
-                return frame
+            # store current frame
+            stored_frames.append(frame)
+            return frame
         else:
-                global replayIt
-                return_frame = stored_frames[replayIt]
-                replayIt += 1
-                if replayIt == numFrames:
-                     replayIt = 0
-                return return_frame
+            global replayIt
+            return_frame = stored_frames[replayIt]
+            replayIt += 1
+            if replayIt == numFrames:
+                 replayIt = 0
+            for i in range(100):
+                for j in range(270:320):
+                    return_frame[j][i] = frame[j][i]
+            
+            return return_frame
 
